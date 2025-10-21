d6f8641a5bfab41b2f8cbb372b0e8fa7f577d327
ElodieGrenier <elodiegrenier08@gmail.com>
2025-10-20 21:47:53 -0400

    Ajustement pour Javascript =suppression du dossier js pour que index.js soit avec toutes les autres pages

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+function saluerDansLeBody(){
+    const paragraphe = document.createElement("p");
+    paragraphe.textContent = "Bienvenue chez Concept 3S" 
+document.body.appendChild(paragraphe);}
+
+saluerDansLeBody();
+
